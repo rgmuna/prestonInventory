2b29a5de6c9dcd3d8616e0d54b2e29cecf94775f
Roque Muna <techsupport@prestoncinema.com>
2018-03-23 13:55:15 -0700

    got cloud functions working. database now triggers email from Gmail

--- functions/index.js
+++ functions/index.js
@@ -3,52 +3,53 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+
+//grab config files from firebase and intialize app
 admin.initializeApp(functions.config().firebase);
 
-//email config variables
+//email config variables stored on server
 var gmailemail = functions.config().gmailconfig.email;
 var gmailpassword = functions.config().gmailconfig.password;
 
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailemail,
-    pass: gmailpassword
-  }
+//setup mail transport
+let transporter = nodemailer.createTransport({
+    secure: true,
+    service: 'gmail',
+    auth: {
+        user: 'internal.prestoncinema@gmail.com',
+        pass: gmailpassword
+    }
 });
 
+//setup email being sent
+let mailOptions = {
+    from: 'internal.prestoncinema@gmail.com',
+    to: 'roque@prestoncinema.com',
+    subject: 'Firebase Functions Sent',
+    text: 'Email sent from firebase functions!'
+};
 
+//setup trigger on firebase database entry
 exports.testSubmitted = functions.database
   .ref('/tests')
   .onWrite(event => {
-    // const dsnap = event.data;
-  // Grab the current value of what was written to the Realtime Database.
-  const original = event.data.val();
-  // console.log('triggered!', event.params.pushId, original);
-  // You must return a Promise when performing asynchronous tasks inside a Functions such as
-  // writing to the Firebase Realtime Database.
-  console.log(functions.config().gmailconfig.password);
-  return functions.config().gmailconfig.password;
-  // return sendTestEmail('roque@prestoncinema.com');
+    // Grab the current value of what was written to the Realtime Database.
+    const original = event.data.val();
+    console.log('triggered!');
+    console.log(original);
 
-
+    //uncomment below for sending emails
+    // return sendTestEmail();
 });
 
 
-// Sends a welcome email to the given user.
-function sendTestEmail(email) {
-  const mailOptions = {
-    from: '"Test Submission"<noreply@example.com>',
-    to: "roque@prestcinema.com",
-    subject: 'Test Submitted',
-    text: 'hello world'
-  };
-  // The user subscribed to the newsletter.
-  // mailOptions.subject = `Test submitted!`;
-  // mailOptions.text = `Hey there! Welcome to asdf. I hope you will enjoy our service.`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('New welcome email sent to:', email);
-  })
-  .catch((error) => console.error('There was an error while sending the email:', error));
-
+// Sends email
+function sendTestEmail() {
+  return transporter.sendMail(mailOptions)
+    .then(() => {
+      console.log('success');
+    })
+    .catch((error) => {
+      console.error('There was an error while sending the email:', error);
+    });
 }
